04c8d425153ca5f5fa1f4b1365abd5ff6eb75959
KristoferMar <kristofer@brostu.dk>
2021-05-24 09:44:21 +0200

    created small section on methods

---
+++ Foundation/Foundation/Basic Topics/Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.Basic_Topics.Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at ({0}, {1}) ", point.X, point.Y);
+        }
+    }
+}
---
+++ Foundation/Foundation/Basic Topics/Methods/Readme.md
@@ -0,0 +1,7 @@
+﻿<h1>Methods</h1>
+
+<h2>Signitures</h2>
+- Name
+- Number of parameters
+
+<h2>The Out Modifier</h2>
